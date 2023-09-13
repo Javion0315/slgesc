26c3394c8081f569dc5987472658340aee9e7100
Baibai Tzeng <baibaitzeng@BaiBai.local>
2023-09-13 13:36:37 +0800

    Sidebar Adjustment

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,59 +1,59 @@
 <template>
-	<div class="absolute inset-0 h-screen top-16 z-50">
-		<div class="bg-dark-black100 w-64 top-0 absolute inset-0 overflow-y-auto">
-			<div
-				class="flex justify-start items-center py-4 pl-6 pr-12 hover:text-dark-yellow cursor-pointer text-base whitespace-nowrap"
-				v-for="item in menuList"
-				:key="item.value"
-				@click="goPath(item)"
-				:class="
-					menuActived === item.value
-						? 'text-dark-yellow200 border-r-4 border-dark-yellow200'
-						: 'text-neutral-300'
-				"
-			>
-				<font-awesome-icon :icon="item.icon" size="lg" />
-				<div class="ml-4">{{ item.label }}</div>
-			</div>
-		</div>
-	</div>
+  <div class="absolute inset-0 top-16 z-50">
+    <div class="bg-dark-black100 w-64 top-0 absolute inset-0 overflow-y-auto">
+      <div
+        class="flex justify-start items-center py-4 pl-6 pr-12 hover:text-dark-yellow cursor-pointer text-base whitespace-nowrap"
+        v-for="item in menuList"
+        :key="item.value"
+        @click="goPath(item)"
+        :class="
+          menuActived === item.value
+            ? 'text-dark-yellow200 border-r-4 border-dark-yellow200'
+            : 'text-neutral-300'
+        "
+      >
+        <font-awesome-icon :icon="item.icon" size="lg" />
+        <div class="ml-4">{{ item.label }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 
 export default {
-	data() {
-		return {
-			menuActived: this.$route.name || "index",
-			menuList: [
-				{ label: "首頁", value: "index", icon: ["fas", "house"] },
-				{
-					label: "區域電力資訊",
-					value: "generating",
-					icon: ["fas", "map-location-dot"],
-				},
-				{
-					label: "再生能源",
-					value: "energy",
-					icon: ["fas", "plug-circle-bolt"],
-				},
-				{ label: "節電成效", value: "saving", icon: ["fas", "battery-full"] },
-				{ label: "友善連結", value: "link", icon: ["fas", "link"] },
-			],
-		};
-	},
-	computed: {
-		...mapGetters(["getToggleState"]),
-	},
-	methods: {
-		goPath(item) {
-			if (window.innerWidth <= 840) {
-				this.$store.dispatch("setMenu", false);
-			}
-			this.menuActived = item.value;
-			this.$router.push({ name: item.value });
-		},
-	},
+  data() {
+    return {
+      menuActived: this.$route.name || "index",
+      menuList: [
+        { label: "首頁", value: "index", icon: ["fas", "house"] },
+        {
+          label: "區域電力資訊",
+          value: "generating",
+          icon: ["fas", "map-location-dot"],
+        },
+        {
+          label: "再生能源",
+          value: "energy",
+          icon: ["fas", "plug-circle-bolt"],
+        },
+        { label: "節電成效", value: "saving", icon: ["fas", "battery-full"] },
+        { label: "友善連結", value: "link", icon: ["fas", "link"] },
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters(["getToggleState"]),
+  },
+  methods: {
+    goPath(item) {
+      if (window.innerWidth <= 840) {
+        this.$store.dispatch("setMenu", false);
+      }
+      this.menuActived = item.value;
+      this.$router.push({ name: item.value });
+    },
+  },
 };
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,42 +1,42 @@
 <template>
-	<div class="bg-dark-black text-white min-h-screen" v-cloak>
-		<div>
-			<Navbar />
-			<div class="flex">
-				<Sidebar class="h-screen sticky top-0" v-if="getToggleState" />
-				<main>
-					<Nuxt
-						:class="[
-							'p-6',
-							getToggleState && windowWidth >= 840 ? 'ml-64' : 'ml-0',
-						]"
-					/>
-				</main>
-			</div>
-		</div>
-	</div>
+  <div class="bg-dark-black text-white min-h-screen" v-cloak>
+    <div>
+      <Navbar />
+      <div class="flex">
+        <Sidebar class="sticky top-0" v-if="getToggleState" />
+        <main>
+          <Nuxt
+            :class="[
+              'p-6',
+              getToggleState && windowWidth >= 840 ? 'ml-64' : 'ml-0',
+            ]"
+          />
+        </main>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 
 export default {
-	data() {
-		return {
-			windowWidth: "",
-		};
-	},
-	mounted() {
-		this.windowWidth = window.innerWidth;
-		if (this.windowWidth <= 840) {
-			this.$store.dispatch("setMenu", false);
-		}
-		window.addEventListener("resize", () => {
-			this.windowWidth = window.innerWidth;
-		});
-	},
-	computed: {
-		...mapGetters(["getToggleState"]),
-	},
+  data() {
+    return {
+      windowWidth: "",
+    };
+  },
+  mounted() {
+    this.windowWidth = window.innerWidth;
+    if (this.windowWidth <= 840) {
+      this.$store.dispatch("setMenu", false);
+    }
+    window.addEventListener("resize", () => {
+      this.windowWidth = window.innerWidth;
+    });
+  },
+  computed: {
+    ...mapGetters(["getToggleState"]),
+  },
 };
 </script>
