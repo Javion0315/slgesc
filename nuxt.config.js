3748b19e5869951df1894790edc3a0f1ae003400
javion0315 <t832566751@gmail.com>
2023-10-17 19:38:18 +0800

    Router Base Dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,12 +67,17 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    extractCSS: true
+    extractCSS: true,
+    publicPath: '/dist/'
   },
 
   server: {
     host: '0.0.0.0'
   },
 
-  publicPath: '/slgesc/'
+  router: {
+    base: '/dist/' // 指定基本路徑
+  },
+
+  // publicPath: '/slgesc/'
 }
